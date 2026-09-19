7ef114ad3d6e2a5282221a5f8ec77bdf5223fa58
allghz16 <andiandika3608>
2026-09-19 16:07:33 +0700

    Add SSL and port configuration for TiDB Cloud

--- config/db.js
+++ config/db.js
@@ -6,6 +6,10 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 4000, // Port wajib TiDB Cloud[cite: 7]
+  ssl: {
+    rejectUnauthorized: false // Menghindari error sertifikat TLS di server cloud[cite: 7]
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
