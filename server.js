dce809ab886d74fd686bdf45ece1197c6d657c4b
allghz16 <andiandika3608>
2026-09-20 18:36:45 +0700

    update serverjs

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://kemenlu-emub.vercel.app', 
+    'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
-// Routes API
+// Routes API (Pastikan baris ini ada agar /api/wishes bisa diakses)
 app.use('/api', wishRoutes);
 
 // Test Endpoint Root
